df5c207bbff0ed8bdafcdacdc8ecfd01032a54a6
A.Bratsuk <a.bratsuk@navitek.com.ru>
2017-02-22 20:59:40 +0300

    change test

--- lecture01/src/test/java/ru/atom/HelloWorldTest.java
+++ lecture01/src/test/java/ru/atom/HelloWorldTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.core.IsNot.not;
 public class HelloWorldTest {
     @Test
     public void getHelloWorld() throws Exception {
-        assertThat(HelloWorld.getHelloWorld(), is(equalTo("Hello, World!")));
+        assertThat(HelloWorld.getHelloWorld(), is(equalTo("Hello, Worldrrr!")));
         assertThat(HelloWorld.getHelloWorld(), is(not(equalTo("Some strange string"))));
     }
 }
